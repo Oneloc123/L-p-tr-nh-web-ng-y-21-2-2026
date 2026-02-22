2210804db03f8db1a7b3039a765e8525cb8847b1
NgocAnh998 <23130017@st.hcmuaf.edu.vn>
2026-02-22 09:15:11 +0700

    add model

---
+++ SaleCar/src/main/java/code/salecar/model/Category.java
@@ -0,0 +1,12 @@
+package code.salecar.model;
+
+import java.util.Date;
+
+public class Category {
+    private int id;
+    private String name;
+    private String description;
+    private String image;
+    private Date createat;
+    private Date updateat;
+}
