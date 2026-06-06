c66addb90661ffff0c279c311c92aca703dcb5e1
NgocAnh998 <23130017@st.hcmuaf.edu.vn>
2026-06-06 10:25:13 +0700

    [FIX] #139 Sửa luồng nhập xuất inventory. Đang bất đồng bộ ở tự dộn trừ khi khi mua và nhập xuất bằng tay thủ công. Giải quyết bằng cách đưa chung về 1 luồng

--- SaleCar/src/main/java/code/salecar/controller/admin/product/api/UpdateProductInventoryServlet.java
+++ SaleCar/src/main/java/code/salecar/controller/admin/product/api/UpdateProductInventoryServlet.java
@@ -5,7 +5,6 @@ import code.salecar.model.product.entity.ActivityLog;
 import code.salecar.model.product.entity.ProductSalesInfo;
 import code.salecar.model.product.entity.ProductVariants;
 import code.salecar.service.file.ActivityLogFileService;
-import code.salecar.service.product.InventoryService;
 import code.salecar.service.product.ProductVariantsService;
 import code.salecar.util.NotificationUtil;
 import jakarta.servlet.*;
@@ -41,30 +40,23 @@ public class UpdateProductInventoryServlet extends HttpServlet {
 
         HttpSession session = request.getSession();
         User user = (User) session.getAttribute("user");
-        ActivityLog log =null;
+        ActivityLog log = null;
+
         switch (type) {
             case "add":
-                current += quantity;
-                variant.setQuantity(current);
-                log = ActivityLog.builder().action("Nhập Hàng").timestamp(new Date()).user(user.getFullname()).details(type+": +"+quantity).build();
-//                System.out.println(log.getAction());
-//                System.out.println(log.getTimestamp());
-//                System.out.println(log.getUser());
-//                System.out.println(log.getDetails());
+                log = ActivityLog.builder().action("Nhập Hàng").timestamp(new Date()).user(user.getFullname()).details(type + ": +" + quantity).build();
                 ls.writeLog(variant.getProductId(), log);
-                pvs.createImportReceipt(variant,user);
+                pvs.createImportReceipt(variant, user, quantity);
                 break;
             case "subtract":
                 if (quantity > current) {
                     NotificationUtil.setError(request.getSession(), "Không thể trừ vượt quá tồn kho");
-                    response.sendRedirect(request.getContextPath()+ "/admin/products/detail?id="+ variant.getProductId());
+                    response.sendRedirect(request.getContextPath() + "/admin/products/detail?id=" + variant.getProductId());
                     return;
                 }
-                current -= quantity;
-                variant.setQuantity(current);
-                log = ActivityLog.builder().action("Xuất hàng").timestamp(new Date()).user(user.getFullname()).details(type+": -"+quantity).build();
+                log = ActivityLog.builder().action("Xuất hàng").timestamp(new Date()).user(user.getFullname()).details(type + ": -" + quantity).build();
                 ls.writeLog(variant.getProductId(), log);
-                pvs.createExportReceipt(variant,user);
+                pvs.createExportReceipt(variant, user, quantity);
                 break;
         }
 
